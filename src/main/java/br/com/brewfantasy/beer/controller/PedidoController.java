b2fba75f84f563efe600b321b1dd1717bc28986c
leonardogloria <contato@leonardogloria.com.br>
2022-08-18 23:31:17 -0300

    Rest TEmplate

--- src/main/java/br/com/brewfantasy/beer/controller/PedidoController.java
+++ src/main/java/br/com/brewfantasy/beer/controller/PedidoController.java
@@ -2,29 +2,32 @@ package br.com.brewfantasy.beer.controller;
 
 import br.com.brewfantasy.beer.model.ImpostoVO;
 import br.com.brewfantasy.beer.model.PedidoVO;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
+import java.util.List;
 
 import static com.fasterxml.jackson.databind.type.LogicalType.Map;
 
 @RestController
 @RequestMapping("/pedido")
 public class PedidoController {
+    @Autowired
+    RestTemplate restTemplate;
 
     @PostMapping
     public void fechaPedido(@RequestBody PedidoVO pedidoVO){
         System.out.println(pedidoVO);
-        RestTemplate restTemplate = new RestTemplate();
-        ImpostoVO impostoVO = restTemplate.postForObject("http://localhost:8081", pedidoVO, ImpostoVO.class);
+        ImpostoVO impostoVO = restTemplate.postForObject("http://FINANCEIRO", pedidoVO, ImpostoVO.class);
         System.out.println(impostoVO);
 
     }
+
 }
